4dba679aa7ce6d6b2d3c7a603f188e9ae24861a5
Jack <skelton.jt9@gmail.com>
2022-01-16 23:57:28 -0500

    added function to get weather icons based off ID

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import WeatherData from "./weather-service.js";
+import CardBuilder from "./CardBuilder.js";
 
 async function makeApiCall(city) {
   const response = await WeatherData.getWeather(city);
@@ -11,17 +12,19 @@ async function makeApiCall(city) {
 
 function getElements(response) {
   if (response) {
-    console.log(response);
+    // console.log(response);
     // response.list.forEach((element) => {
     //   $("#showWeather").append(element.main.temp);
     //   console.log(element.main.temp);
     // });
-    for (let i = 0; i <= 7; i++) {
-      $("#showWeather").append(" " + response.list[i].main.temp);
-    }
+    // for (let i = 0; i <= 7; i++) {
+    // }
+    // $("#showWeather").append(" " + response.list[5].main.temp);
     // let weatherData = JSON.stringify(response.list[0].main.temp);
     // $("#showWeather").text(weatherData);
     // console.log(weatherData);
+    let temp = CardBuilder.buildCard(response);
+    $("#showWeather").append(temp);
   } else {
     console.log("Error");
   }
@@ -32,6 +35,7 @@ function getElements(response) {
 $(document).ready(function () {
   $("#checkWeatherButton").click(function () {
     let city = $("#cityInput").val();
+    $("#showWeather").html("");
     makeApiCall(city);
   });
 });
