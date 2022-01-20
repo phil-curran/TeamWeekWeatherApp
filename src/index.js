ea265eda5c76774464ae03d9c04a3c5e14688280
Jack <skelton.jt9@gmail.com>
2022-01-20 00:02:26 -0500

    current build

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "./css/styles.css";
 import WeatherData, { WeatherData2 } from "./weather-service.js";
 import CardBuilder from "./CardBuilder.js";
 
 async function makeApiCall(city) {
-  const response = await WeatherData.getWeather(city);
   const response2 = await WeatherData2.getWeather(city);
-  getElements(response);
+  const response = await WeatherData.getWeather(city);
+  getElements(response2, response);
   console.log(response2);
 }
 
-function getElements(response) {
-  if (response) {
-    CardBuilder.buildCard(response);
+function getElements(response2, response) {
+  if ((response2, response)) {
+    CardBuilder.buildCard(response2, response);
   } else {
     console.log("Error");
   }
